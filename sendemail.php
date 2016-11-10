1bb5fc4b6c85ab2766face39b36b5016a5678403
jaelee0409 <jaelee0409@gmail.com>
2016-11-10 14:35:25 -0600

    Send Email

---
+++ sendemail.php
@@ -0,0 +1,79 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+	<title>Sendemail Script</title>
+
+</head>
+
+<body>
+
+	<!-- Reminder: Add the link for the 'next page' (at the bottom) -->
+	<!-- Reminder: Change 'YourEmail' to Your real email -->
+
+	<?php
+
+	$ip = $_POST['ip'];
+	$httpref = $_POST['httpref'];
+	$httpagent = $_POST['httpagent'];
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$message = $_POST['message'];
+
+	if(!$visitormail == "" && (!strstr($visitormail,"@") || !strstr($visitormail,".")))
+	{
+	echo "<h2>Use Back - Enter valid e-mail</h2>\n";
+	$badinput = "<h2>Feedback was NOT submitted</h2>\n";
+	echo $badinput;
+	die ("Go back! ! ");
+}
+
+if(empty($name) || empty($email) || empty($message)) {
+echo "<h2>Use Back - fill in all fields</h2>\n";
+die ("Use back! ! ");
+}
+
+$todayis = date("l, F j, Y, g:i a") ;
+
+$subject = $name;
+
+$message = stripcslashes($message);
+
+$message = " $todayis [EST] \n
+Message: $message \n
+From: $name ($email)\n
+Additional Info : IP = $ip \n
+Browser Info: $httpagent \n
+Referral : $httpref \n
+";
+
+$from = "From: $email\r\n";
+
+
+mail("jaelee0409@gmail.com", $subject, $message, $from);
+
+?>
+
+<p align="center">
+	Date: <?php echo $todayis ?>
+	<br />
+	Thank You : <?php echo $name ?> ( <?php echo $email ?> )
+	<br />
+
+	<br />
+	Message:<br />
+	<?php $messageout = str_replace("\r", "<br/>", $message);
+	echo $messageout; ?>
+	<br />
+	<?php echo $ip ?>
+
+	<br /><br />
+	<a href="https://jaelee0409.github.io/a"> Next Page </a>
+</p> 
+
+</body>
+
+</html>
