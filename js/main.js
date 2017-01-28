33607272179f5a5270ac6cb24cc4507c90e00df7
jaelee0409 <jaelee0409@gmail.com>
2017-01-28 02:25:05 -0600

    v.2.1 email test

--- js/main.js
+++ js/main.js
@@ -227,7 +227,7 @@
 			$.ajax({      	
 
 		      type: "POST",
-		      url: "inc/sendEmail.php",
+		      url: "sendEmail.php",
 		      data: $(form).serialize(),
 		      beforeSend: function() { 
 
