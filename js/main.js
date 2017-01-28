d8f021807528c28586de236feb18d61a522856de
jaelee0409 <jaelee0409@gmail.com>
2017-01-28 03:57:04 -0600

    v.3.1
    Email fix

--- js/main.js
+++ js/main.js
@@ -227,7 +227,7 @@
 -			$.ajax({      	
 -
 -		      type: "POST",
--		      url: "sendEmail.php",
+-		      url: "inc/sendEmail.php",
 -		      data: $(form).serialize(),
 -		      beforeSend: function() { 
 -
