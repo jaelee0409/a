758056882e9f45e2ccfe25679bde3f2fb7dab5cf
Jae Lee <jaeslee@utexas.edu>
2019-06-10 14:31:10 -0500

    Graduation Update

--- js/main.js
+++ js/main.js
@@ -140,24 +140,25 @@
    var toggleButton = $('.menu-toggle'),
        nav = $('.main-navigation');
 
-   // toggle button
-   toggleButton.on('click', function(e) {
+   nav.slideToggle();
+   // // toggle button
+   // toggleButton.on('click', function(e) {
 
-		e.preventDefault();
-		toggleButton.toggleClass('is-clicked');
-		nav.slideToggle();
+		// e.preventDefault();
+		// toggleButton.toggleClass('is-clicked');
+		// nav.slideToggle();
 
-	});
+	// });
 
-   // nav items
-  	nav.find('li a').on("click", function() {   
+   // // nav items
+  	// nav.find('li a').on("click", function() {   
 
-   	// update the toggle button 		
-   	toggleButton.toggleClass('is-clicked'); 
-   	// fadeout the navigation panel
-   	nav.fadeOut();   		
+   // 	// update the toggle button 		
+   // 	toggleButton.toggleClass('is-clicked'); 
+   // 	// fadeout the navigation panel
+   // 	nav.fadeOut();   		
    	     
-  	});
+  	// });
 
 
    /*---------------------------------------------------- */
@@ -187,7 +188,7 @@
 	});
 
 
-	/*---------------------------------------------------- */
+    /*---------------------------------------------------- */
   	/* Smooth Scrolling
   	------------------------------------------------------ */
   	$('.smoothscroll').on('click', function (e) {
